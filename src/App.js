074b98dd664016b8bf5d000843e72f9651666613
taufique100 <taufiqueali2911@gmail.com>
2023-04-10 15:59:15 +0530

    React-Router complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-//  import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+        {/* <Home/> */}
+      <h1>This is a react App</h1>
+        <Routes>
+          <Route exact path="/" element={ <Home/>} />
+          <Route exact path="/about" element={ <About/>} />
+        </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+     <h4>This is home</h4> 
+    </div>
+  )
+}
+
+export default Home
